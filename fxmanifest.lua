d978c34922c64cfbcce4a41288f9e84f18a672c9
MoneSuper <mome9132@gmail.com>
2022-09-28 21:18:53 +0300

    psr-input

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+game 'rdr3'
+fx_version 'adamant'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'psr-input'
+version '0.0.1'
+
+client_scripts {
+    'client/*.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/styles/*.css',
+    'html/script.js'
+}
+
+lua54 'yes'
